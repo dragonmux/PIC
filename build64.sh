b65b2f311fc18ae44e5556ca675bf46722663d37
dx-mon <dx-mon@users.sourceforge.net>
2013-04-22 00:44:49 +0100

    Added LGPL notices to the code ready for release

--- build64.sh
+++ build64.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # This file is part of PIC (libGTK++)
 # Copyright © 2012 Rachel Mant (dx-mon@users.sourceforge.net)
 #
@@ -14,33 +16,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-MAKEFLAGS = -w -r -R
-GCC ?= gcc
-ifeq ($(strip $(DEBUG)), 1)
-	OPTIM_FLAGS = -ggdb
-	GCC_FLAGS = 
-else
-	GCC_VER = $(shell gcc -dumpversion | cut -d . -f 1)
-	ifeq ($(shell if [ $(GCC_VER) -ge 4 ]; then echo 1; else echo 0; fi), 1)
-		GCC_FLAGS = -fvisibility=hidden -fvisibility-inlines-hidden
-	else
-		GCC_FLAGS = 
-	endif
-	OPTIM_FLAGS = -O2
-	DEBUG = 0
-endif
-CC = $(GCC) $(GCC_FLAGS)
-CFLAGS = $(OPTIM_FLAGS) -Wall -c -o $@ $<
-LFLAGS = 
-
-O = ROM.o IntelHex.o Memory16F88.o 16F88.o
-
-default: all
-
-all: $(O)
-
-.cpp.o:
-	$(CC) $(CFLAGS)
-
-clean:
-	rm -f *.o
+GCC="gcc -m64 -fPIC -DPIC" make "$@"
